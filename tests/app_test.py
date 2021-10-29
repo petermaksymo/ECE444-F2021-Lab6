cddbfa5c463febf7cd1fb9c1b6bd0db16c084a56
petermaksymo <peter.maksymowsky@mail.utoronto.ca>
2021-10-29 12:58:55 -0400

    Initial commit and initialized db

---
+++ tests/app_test.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+from project.app import app, init_db
+
+
+def test_index():
+    tester = app.test_client()
+    response = tester.get("/", content_type="html/text")
+
+    assert response.status_code == 200
+    assert response.data == b"Hello, World!"
+
+
+def test_database():
+    init_db()
+    assert Path("flaskr.db").is_file()
